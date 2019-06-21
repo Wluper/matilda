bf821de13de01ae5338604f196752ddcd1b6c790
Nikolai Rozanov <nikolai.rozanov@gmail.com>
2019-06-21 01:26:00 +0100

    a bit more manual testing of interannotator

--- tests/test_server.py
+++ tests/test_server.py
@@ -15,13 +15,33 @@ sys.path.append(path + "/../server")
 
 # == Local ==
 import app
+import lida_app as lida
 from annotator_config import Configuration
-
+from interannotator_config import agreementConfig
 
 ##############################################
 #  CODE
 ##############################################
+classificationList = [
+    ["a","b"],
+    ["a","c"]
+]
 
+classificationList2 = [
+    ["a","b"],
+    ["a","b"]
+]
+
+
+clsStringList = [
+    [("a","Hi"), ("b","Hi")],
+    [("b", "Bye"), ("c", "Bye")]
+]
+
+clsStringList2 = [
+    [("a","Hi"), ("b","Hi")],
+    [("a", "Bye"), ("b", "Bye")]
+]
 
 
 ##############################################
@@ -30,9 +50,14 @@ from annotator_config import Configuration
 
 if __name__ == '__main__':
 
-    print( json.dumps(app.LidaAppWrapper.run_models_on_query("Hi"), indent=4 ) )
+    # print( json.dumps( agreementConfig["multilabel_classification"](classificationList2) , indent=4 ) )
 
-    print( json.dumps(Configuration.create_annotation_dict(), indent=4 ) )
+    print( json.dumps( agreementConfig["multilabel_classification_string"](clsStringList) , indent=4 ) )
+
+
+    # print( json.dumps(lida.LidaApp.run_models_on_query("Hi"), indent=4 ) )
+
+    # print( json.dumps(Configuration.create_annotation_dict(), indent=4 ) )
 
 
 
