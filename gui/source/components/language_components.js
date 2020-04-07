1ac3de451cdf31e29d1b9ff194b7efc4a3b07588
Davide Cucurnia <davidecucurnia@MBP-di-Davide.homenet.telecomitalia.it>
2020-04-07 21:23:29 +0200

    language detection

--- gui/source/components/language_components.js
+++ gui/source/components/language_components.js
@@ -2,6 +2,8 @@
 * Language Options
 *************************************/
 
+guiMessages.selected = guiMessages.en;
+
 var languageSection = new Vue({
   el:"#language-selector",
   
@@ -13,8 +15,22 @@ var languageSection = new Vue({
     currentLanguage: "en",
     guiMessages,
   },
+
+  mounted() {
+    this.detectLanguage();
+  },
   
   methods: {
+
+    detectLanguage() {
+      var browserLanguage = navigator.language.substr(0,2);
+      if (guiMessages[browserLanguage] == undefined)
+        guiMessages.selected = guiMessages.en;
+      else 
+        guiMessages.selected = guiMessages[browserLanguage];
+      this.currentLanguage = browserLanguage;
+    },
+
     updateGui(event) {
       console.log('---- CHANGING GUI LANGUAGE ----');
       guiMessages.selected = guiMessages[this.currentLanguage];
