ca91efb49770dafc3ad668f060a2f5252d2a9ec4
davivcu <dav.iv.kovac@gmail.com>
2020-10-26 00:07:53 +0100

    docker support

--- web/database_init.py
+++ web/database_init.py
@@ -6,5 +6,6 @@ users = db["users"]
 values = {"id":"admin","userName":"admin","password":"admin","email":"","role":"administrator"}
 if db.users.find({"id":"admin"}):
     print("Admin account exists already")
-db.users.insert_one(values)
+else:
+	db.users.insert_one(values)
 print("Success! log with username 'admin' and password 'admin'")
